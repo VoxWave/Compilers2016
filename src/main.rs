038d775fde3e4c5105bcf05bb072f471994ae173
VoxWave <victor_bankowski@hotmail.com>
2016-02-20 11:22:37 +0200

    started with commandline arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    //let f = try!(File::open());
+    let mut path = String::new();
+
+    for (i, argument) in env::args().enumerate() {
+        if i == 1 {
+            path = argument.to_string();
+        }
+    }
+
+    if path.is_empty() {
+        panic!("No path to a source file provided.")
+    }
 }
