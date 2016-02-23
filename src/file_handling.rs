f2c5aa3e5022080e8b4b604090e0bbca5f736bc4
VoxWave <victor_bankowski@hotmail.com>
2016-02-23 19:41:31 +0200

    filehandling is about done. also made some more module files.

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -1,8 +1,29 @@
-fn get_source_file() -> Result<File, String> {
+use std::env;
+use std::fs::File;
 
+fn get_source_text() -> String {
+    let path = get_path();
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(file) => file,
+    }
+    let mut source_text = String::new();
+    // I should read the file to a string until a ';' character come up and then start a new string
+    // and store all the resulting strings in a Vec or some other data structure. I don't know how
+    // to do that in rust yet and that's why I just read the whole source file into a string and
+    // chop up the string into smaller strings afterwards.
+    match file.read_to_string(&mut source_text) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   Error:description(&why)),
+        Ok(_) => {}
+    }
+    source_text
 }
 
-fn get_path() -> String {
+fn get_path() -> Path {
     //let f = try!(File::open());
     let mut path = String::new();
 
@@ -14,6 +35,6 @@ fn get_path() -> String {
 
     if path.is_empty() {
         panic!("No path to a source file provided.");
-        path
     }
+    path
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
-use std::fs::File;
-
 mod file_handling;
+mod source_processing;
+mod interpreter;
 
 fn main() {
-    let mut file = file_handling::get_source_file();
+    let mut raw_source = file_handling::get_source_file();
+    let mut lines = source_processing::process_raw_source(raw_source);
 }
