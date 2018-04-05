ba4fcdaabdef34f3da20321a7a9e49d6556eb676
VoxWave <victor_bankowski@hotmail.com>
2018-04-05 12:42:30 +0300

    used channels to stop the program instead of an abort

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,8 @@ fn main() {
     let pool = ThreadPoolBuilder::new().num_threads(3).build().unwrap();
     let (mut token_sink, mut token_source) = channel();
     let (mut statement_sink, mut statement_source) = channel();
+    let (mut kill_switch, mut kill_signal) = channel();
+
     pool.spawn(move || {
         let mut scanner = Scanner::new();
         scanner.scan(&file_handling::get_source_text(), &mut token_sink);
@@ -36,7 +38,7 @@ fn main() {
     pool.spawn(move || {
         let mut interpreter = Interpreter::new();
         interpreter.interpret(&mut statement_source);
-        abort()
+        kill_switch.send(true);
     });
-    loop {}
+    kill_signal.recv();
 }
